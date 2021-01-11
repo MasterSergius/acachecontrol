eb5773ad2dd20210ed91a14ef35595cc5b4240af
Serhii Buniak <master.sergius@gmail.com>
2021-01-11 16:03:13 +0200

    Fix lint errors.

--- tests/unit/test_request_context_manager.py
+++ tests/unit/test_request_context_manager.py
@@ -3,21 +3,17 @@ from acachecontrol.request_context_manager import RequestContextManager
 
 
 def test_init(mocker):
-    method = 'GET'
-    url = 'http://example.com'
+    method = "GET"
+    url = "http://example.com"
     timeout = 10
     cache = AsyncCache()
-    rcm = RequestContextManager(mocker.Mock(),
-                                cache,
-                                method,
-                                url,
-                                timeout=timeout,
-                                allow_redirects=True
-                                )
+    rcm = RequestContextManager(
+        mocker.Mock(), cache, method, url, timeout=timeout, allow_redirects=True
+    )
 
     assert rcm.method == method
     assert rcm.url == url
     assert rcm.key == (method, url)
-    assert rcm.params == {'timeout': timeout, 'allow_redirects': True}
+    assert rcm.params == {"timeout": timeout, "allow_redirects": True}
     assert rcm.cache == cache
     assert rcm.cache.wait_timeout == timeout
