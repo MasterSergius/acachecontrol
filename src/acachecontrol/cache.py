1286c80eeaf0e6409af6e8291a1d4641361854bc
Andrii Ivaniuk <andrii.ivaniuk@stem.com>
2021-01-11 16:45:08 +0200

    Updated tests

--- src/acachecontrol/cache.py
+++ src/acachecontrol/cache.py
@@ -32,14 +32,14 @@ class AsyncCache:
 
     def __init__(self, config: Dict = None):
         self.cache = {}  # type: Dict
+        config = config or {}
         self._wait_until_completed = set()  # type: Set
-        if config:
-            self.default_max_age = config.get("max_age", DEFAULT_MAX_AGE)
-            self.wait_timeout = config.get("wait_timeout", DEFAULT_WAIT_TIMEOUT)
-            self.sleep_time = config.get("sleep_time", DEFAULT_SLEEP_TIME)
-            self.cacheable_methods = config.get(
-                "cacheable_methods", CACHEABLE_METHODS
-            )
+        self.default_max_age = config.get("max_age", DEFAULT_MAX_AGE)
+        self.wait_timeout = config.get("wait_timeout", DEFAULT_WAIT_TIMEOUT)
+        self.sleep_time = config.get("sleep_time", DEFAULT_SLEEP_TIME)
+        self.cacheable_methods = config.get(
+            "cacheable_methods", CACHEABLE_METHODS
+        )
 
     def has_valid_entry(self, key: Tuple[str, str]) -> bool:
         """Check if entry exists and not expired, delete expired."""
--- tests/unit/test_acachecontrol.py
+++ tests/unit/test_acachecontrol.py
@@ -4,31 +4,6 @@ from acachecontrol import AsyncCacheControl
 
 
 @pytest.mark.asyncio
-async def test_request(mocker):
-    """Verify AsyncCacheControl.request works correctly."""
-    mock_RCM = mocker.Mock()
-    async with AsyncCacheControl(
-        request_context_manager_cls=mock_RCM
-    ) as cached_sess:
-        _ = cached_sess.get("http://example.com")
-        mock_RCM.assert_called_with(
-            cached_sess._async_client_session,
-            cached_sess.cache,
-            "GET",
-            "http://example.com",
-        )
-
-        _ = cached_sess.poet("http://example.com", data={"key": "value"})
-        mock_RCM.assert_called_with(
-            cached_sess._async_client_session,
-            cached_sess.cache,
-            "POST",
-            "http://example.com",
-            data={"key": "value"},
-        )
-
-
-@pytest.mark.asyncio
 async def test_head(mocker):
     """Verify AsyncCacheControl.head works correctly."""
     mock_RCM = mocker.Mock()
