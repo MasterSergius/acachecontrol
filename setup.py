62e614ee6fd4308c3cb82c94e572cea20e610860
Vira Buniak <virynka.b@gmail.com>
2021-06-24 17:06:32 +0300

    Add support python 3.9

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
@@ -54,6 +55,6 @@ setup(
         'Changelog': 'https://github.com/MasterSerius/acachecontrol/blob/master/CHANGELOG.md',
         'Issue Tracker': 'https://github.com/MasterSergius/acachecontrol/issues',
     },
-    python_requires='>=3.6,<3.9',
+    python_requires='>=3.6,<4.0',
     install_requires=requires,
 )
